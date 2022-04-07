20c73918bbdcfde946f810bda7865d305bdf051b
Błażej Skrzypniak <blazej.skrzypniak@com40.pl>
2022-04-07 07:44:55 +0200

    Wyświetlenie pojedyńczego posta

--- src/app/post/show-post/show-post.component.ts
+++ src/app/post/show-post/show-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from 'src/app/shared/models/post';
+import { PostService } from 'src/app/shared/services/post/post.service';
 
 @Component({
   selector: 'app-show-post',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-post.component.scss']
 })
 export class ShowPostComponent implements OnInit {
+  id!: string;
 
-  constructor() { }
+  post!: any;
+
+  constructor(
+    private postService: PostService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.post = this.postService.getPostById(Number(this.id));
+    console.log(this.post);
   }
 
 }
